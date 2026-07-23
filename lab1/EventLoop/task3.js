fdc543c42961255b7d8ca7753c606a8189fd90f7
Daksh <d8550968@gmail.com>
2026-07-23 16:24:11 +0530

    changed the number

--- lab1/EventLoop/task3.js
+++ lab1/EventLoop/task3.js
@@ -2,4 +2,4 @@ console.log("1: start");
 setTimeout(()=> console.log("2: setTimeout"),0);
 setImmediate(() => console.log("3: setImmediate")); 
 process.nextTick(() => console.log("4: nextTick"));
-console.log("4:end");
+console.log("5:end");
