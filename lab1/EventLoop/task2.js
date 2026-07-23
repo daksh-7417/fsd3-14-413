612472f06b9b3ce8341cf9496102ef350fd5824a
Daksh <d8550968@gmail.com>
2026-07-23 16:22:14 +0530

    timer

---
+++ lab1/EventLoop/task2.js
@@ -0,0 +1,29 @@
+import fs from 'fs/promises';
+const writeData = async ()=>{
+    try{
+        console.log('about to write....');
+        await fs.writeFile('stud.txt',"Name:Raman Singh");
+        console.log("filewritten");
+    }catch(error)
+    {console.log(error);}
+};
+const f1 = () =>{
+    console.log("f1");
+};
+const f2 = () => {
+    console.log("f2");
+};
+const f3 = () => {
+    console.log("f3");
+};
+
+const main = () => {
+    console.log("main");
+    setTimeout(f1, 0);
+    setInterval(f2,1000);        // press ctrl + c to stop
+    setImmediate(f2);
+    process.nextTick(f3);
+    writeData();       
+    console.log("end");
+};
+main();
