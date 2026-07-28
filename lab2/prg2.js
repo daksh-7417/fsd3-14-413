3a7e408e63f484a72a29b1e027f33f8e49e0b8f7
Daksh <d8550968@gmail.com>
2026-07-28 10:27:42 +0530

    folder creation and deletion

---
+++ lab2/prg2.js
@@ -0,0 +1,8 @@
+import {mkdir, rm} from "fs/promises";
+// await mkdir("uploads");
+// await mkdir("uploads/images");
+// await mkdir("docs/resumes/data",{recursive: true});
+// await rm("docs/resumes/data",{recursive:true});
+
+// removes main folder and sub folder also
+await rm("docs",{recursive:true});
