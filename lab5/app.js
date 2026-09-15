e29b5eaecd8324477dd03d593fa8dac0786de599
Daksh <d8550968@gmail.com>
2026-09-15 10:32:43 +0530

    end of the day

--- lab5/app.js
+++ lab5/app.js
@@ -2,8 +2,9 @@ import express from 'express'
 
 const app = express();
 
-app.get("/", (req,res)=>{
+app.get("", (req,res)=>{
     res.send("<h1> Hello Express");
 });
+
 
 app.listen(3000,()=> console.log("Server is running through express"));
