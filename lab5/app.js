41cd9a3e8be0947a22b8a995391cea28d7597fd5
Daksh <d8550968@gmail.com>
2026-09-15 10:26:10 +0530

    express introduction

---
+++ lab5/app.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("<h1> Hello Express");
+});
+
+app.listen(3000,()=> console.log("Server is running through express"));
