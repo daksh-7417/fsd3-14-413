5d091db75a576c3de28ab7b2b25e6335194b1aef
Daksh <d8550968@gmail.com>
2026-09-17 13:59:20 +0530

    basic routing with express server

--- lab5/app.js
+++ lab5/app.js
@@ -2,9 +2,35 @@ import express from 'express'
 
 const app = express();
 
-app.get("", (req,res)=>{
+app.get("/", (req,res)=>{
     res.send("<h1> Hello Express");
 });
 
+app.get('/about',(req,res)=>{
+    res.send("We are FSD Developer")
+})
 
-app.listen(3000,()=> console.log("Server is running through express"));
+app.post('/login',(req,res)=>{
+    res.send({msg:'user login'})
+})
+
+app.put('/user/update/1',(req,res)=>{
+    res.send({msg:'user update'})
+})
+
+app.delete('/users/1',(req,res)=>{
+    res.send({msg:'remove user 1'})
+})
+
+app.use((req,res)=>{
+    res.status(404).send("Not found")
+})
+
+
+app.listen(3333,()=> console.log("Server is running through express"));
+
+// server.on("error", (err) => {
+//     console.error("Server listen error:",err);
+// })
+
+
